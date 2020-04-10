6756784a736267729fe05387fbf157b008c6cf18
空空如也 <shift_1220@163.com>
2020-04-10 18:35:15 +0800

    detail.html增加markdown语法支持

--- blog/views.py
+++ blog/views.py
@@ -14,5 +14,9 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.body = markdown.markdown(post.body,
-                                  )
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', locals())
