63d42d428521979e079e855cc54bfb23c1f15e38
空空如也 <shift_1220@163.com>
2020-04-21 08:58:24 +0800

    增加标签和分类下的统计功能

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 # _*_ coding:utf-8 _*_
 from django import template
 from ..models import Post, Category, Tag
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -20,6 +21,7 @@ def show_archives(context):
         'date_list': Post.objects.dates('created_time', 'month', order='DESC'),
     }
 
+
 # 显示文章分类
 @register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
 def show_categories(context):
@@ -27,9 +29,26 @@ def show_categories(context):
         'category_list': Category.objects.all(),
     }
 
+
 # 显示文章标签
 @register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
 def show_tags(context):
     return {
         'tag_list': Tag.objects.all(),
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
+def show_categories(context):
+    category_list = Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+    return {
+        'category_list': category_list,
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
+def show_tags(context):
+    tag_list = Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+    return {
+        'tag_list': tag_list,
     }
